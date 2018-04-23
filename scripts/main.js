8d5a556ec6d620d5f2d83403edbc43fba2455d81
Ludvig pr_ Åslund <ludas.aslund@outlook.com>
2018-04-23 14:33:52 +0200

    Update frontend stuff

--- scripts/main.js
+++ scripts/main.js
@@ -1,12 +1,4 @@
 $(document).ready(function(){
-    $('.index-map').on('click', function() {
-        window.location.href = "http://users.du.se/~h16ludas/ik1056/geoProjekt/map.html";
-    });
-    /*
-    $('.index-login').on('click', function() {
-        window.location.href = "http://users.du.se/~h16ludas/ik1056/geoProjekt/admin.php";
-    })
-    */
 });
 function initMap() {
     var uluru = {lat: 37.795112, lng: 13.4843497};
