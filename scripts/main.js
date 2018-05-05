521b3cf94d75e0eac2a06ffb0c3c11a8be01d78f
Ludvig pr_ Åslund <ludas.aslund@outlook.com>
2018-05-05 18:36:44 +0200

    Add RSS-feed to map and show markers from feed.

--- scripts/main.js
+++ scripts/main.js
@@ -63,6 +63,32 @@ function initMap() {
     loadDataFromDB();
 }
 
+function getWeatherData(feature){
+    var lat = feature.getGeometry().get().lat();
+    var lng = feature.getGeometry().get().lng();
+    var info = feature.getProperty("information");
+
+    $.ajax({
+        type: "POST",
+        url: "./php/route.php",
+        data: {"controller":"Controller","function":"getCurrentWeather", "lat":lat, "lng":lng},
+        dataType: "json",
+        success:function(data){
+            var content = "<span>Information:" + feature.getProperty('information') + "</span><br>"+
+                "<span>Väder:</span><img src=" + data.weather.icon + " style='width: 25px; height: 25px;'/>" +
+                "<span>"+data.temperature.temp+"</span>";
+
+            infowindow = new google.maps.InfoWindow({
+                content: content,
+                options: {pixelOffset: new google.maps.Size(0, -30)},
+                position: feature.getGeometry().get()
+            });
+
+            infowindow.open(map);
+        }
+    });
+}
+
 function loadDataFromDB(){
     $.ajax({
         type: "POST",
@@ -90,16 +116,197 @@ function setMarkers(){
         if(infowindow) {
             infowindow.close();
         }
-        console.log(e.feature.getProperty('information'));
-        infowindow = new google.maps.InfoWindow({
-            content: e.feature.getProperty('information'),
-            options: {pixelOffset: new google.maps.Size(0, -30)},
-            position: e.feature.getGeometry().get()
-        });
-        infowindow.open(map);
+        getWeatherData(e.feature);
     });
 }
 
 $(document).ready(function(){
+    $.support.cors = true;
+    var test =
+        "<REQUEST>" +
+            "<LOGIN authenticationkey='88597617d8524d5e9baf509b2b92a968' />" +
+            "<QUERY objecttype='Situation' limit='10'>"+
+                "<FILTER>"+
+                    "<OR>"+
+                        "<ELEMENTMATCH>"+
+                            "<EQ name='Deviation.ManagedCause' value='true'/>"+
+                            "<IN name='Deviation.MessageType' value='Trafikmeddelande,Olycka' />"+
+                        "</ELEMENTMATCH>"+
+                        "<ELEMENTMATCH>"+
+                            "<EQ name='Deviation.MessageType' value='Färjor' />"+
+                            "<EQ name='Deviation.IconId' value='ferryServiceNotOperating' />"+
+                        "</ELEMENTMATCH>"+
+                        "<ELEMENTMATCH>"+
+                            "<EQ name='Deviation.MessageType' value='Restriktion' />"+
+                            "<EQ name='Deviation.MessageCode' value='Väg avstängd' />"+
+                        "</ELEMENTMATCH>"+
+                            "<ELEMENTMATCH>"+
+                            "<EQ name='Deviation.MessageType' value='Vägarbete'/>"+
+                            "<EQ name='Deviation.SeverityCode' value='5'/>"+
+                        "</ELEMENTMATCH>"+
+                        "<ELEMENTMATCH>"+
+                            "<NE name='Deviation.MessageType' value='Vägarbete' />"+
+                            "<GTE name='Deviation.SeverityCode' value='4' />"+
+                        "</ELEMENTMATCH>"+
+                    "</OR>"+
+                "</FILTER>"+
+            "</QUERY>"+
+        "</REQUEST>";
+    $.ajax({
+       type: "POST",
+       url: "http://api.trafikinfo.trafikverket.se/v1.3/data.json",
+       contentType: "text/xml",
+       dataType: "json",
+       data: test,
+       success: function(data){
+           console.log(data);
+           var rs = data.RESPONSE.RESULT[0].Situation;
+           for (var i = 0; i < rs.length; i++) {
+               var timePosted = rs[i].Deviation[0].CreationTime;
+               var start, end;
+               start = rs[i].Deviation[0].StartTime.split('T')[0];
+               try{
+                   end = rs[i].Deviation[0].EndTime.split('T')[0];
+               }catch(e){
+                   end = undefined;
+               }
 
+               var county = rs[i].Deviation[0].CountyNo[0];
+               switch(county){
+                   case 0:
+                       county = "Hela Sverige";
+                   break;
+                   case 1:
+                       county = "Stockholms Län";
+                   break;
+                   case 2:
+                       county = "Stockholms Län";
+                       break;
+                   case 3:
+                       county = "Uppsala län";
+                       break;
+                   case 4:
+                       county = "Södermanlands län";
+                       break;
+                   case 5:
+                       county = "Östergötlands län";
+                       break;
+                   case 6:
+                       county = "Jönköpings län";
+                       break;
+                   case 7:
+                       county = "Kronobergs län";
+                       break;
+                   case 8:
+                       county = "Kalmar län";
+                       break;
+                   case 9:
+                       county = "Gotlands län";
+                       break;
+                   case 10:
+                       county = "Blekinge län";
+                       break;
+                   case 12:
+                       county = "Skåne län";
+                       break;
+                   case 13:
+                       county = "Hallands län";
+                       break;
+                   case 14:
+                       county = "Västra Götalands län";
+                       break;
+                   case 17:
+                       county = "Värmlands län";
+                       break;
+                   case 18:
+                       county = "Örebro län";
+                       break;
+                   case 19:
+                       county = "Västmanlands län";
+                       break;
+                   case 20:
+                       county = "Dalarnas län";
+                       break;
+                   case 21:
+                       county = "Gävleborgs län";
+                       break;
+                   case 22:
+                       county = "Västerbottens län";
+                       break;
+                   case 23:
+                       county = "Jämtlands län";
+                       break;
+                   case 24:
+                       county = "Västerbottens län";
+                       break;
+                   case 25:
+                       county = "Norrbottens län";
+                       break;
+            }
+               var html = '<div class="traffic-news-item">';
+               html += '<div class="traffic-rss-header">';
+               html += '<span class="traffic-rss-headline">';
+               html += county;
+               html += '</span>';
+               html += '<span class="traffic-news-posted">';
+               if(end !== undefined)
+                    html += start + " - " + end;
+               else
+                   html += start;
+               html += '</span>';
+               html += '</div>';
+               if(rs[i].Deviation[0].RoadNumber !== undefined)
+                    html += '<span>'+ rs[i].Deviation[0].RoadNumber +'</span>'
+               html += '<div class="traffic-rss-message">';
+               html += rs[i].Deviation[0].Message;
+               html += '</div>';
+               html += '<div class="traffic-rss-footer">';
+               html += '       </div>';
+               html += '</div>';
+               $('#news-rss').append(html);
+
+               if(rs[i].Deviation[0].Geometry.WGS84 !== undefined) {
+
+                   var geojsonObject = $.geo.WKT.parse(rs[i].Deviation[0].Geometry.WGS84);
+
+                   var geojson = {};
+                   geojson['type'] = 'FeatureCollection';
+                   geojson['features'] = [];
+                   var newFeature = {
+                       "type":"Feature",
+                       "geometry":{
+                           "type":geojsonObject.type,
+                           "coordinates":geojsonObject.coordinates
+                       },
+                       "properties":{
+                           "information":rs[i].Deviation[0].Message
+                       }
+                   }
+                   geojson['features'].push(newFeature);
+                   map.data.addGeoJson(geojson);
+               }
+           }
+       },
+        error: function(data){
+           console.log(JSON.stringify(data));
+        }
+    });
+    showWeatherWidget(60.4866813, 15.4060031);
 });
+
+function showWeatherWidget(lat, lng){
+    $.ajax({
+        type: "POST",
+        url: "./php/route.php",
+        data: {"controller":"Controller","function":"getCurrentWeather","lat":lat,"lng":lng},
+        dataType: "json",
+        success: function(data){
+            console.log(data);
+
+            $('#weather').append("<img src='" + data.weather.icon + "' style='width:40px;'><span>" + data.weather.desc+ "</span> <span>" + data.temperature.temp + "&deg;C</span>");
+        },
+        error: function(data){
+            console.log(JSON.stringify(data));
+        }
+    });
+}
--- scripts/rss.js
+++ scripts/rss.js
@@ -16,25 +16,25 @@ function getTrafficNews() {
         data: request,
         success: function (data) {
             var rs = data.RESPONSE.RESULT[0].Situation;
-            console.log(rs);
+            //console.log(rs);
             for (var i = 0; i < rs.length; i++) {
                 var timePosted = rs[i].Deviation[0].CreationTime;
                 var time = timePosted.split("T");
                 var html = '<div class="traffic-news-item">';
-                html += '<div class="traffic-rss-header">';
-                html += '<span class="traffic-rss-headline">';
-                html += rs[i].Deviation[0].Creator;
-                html += '</span>';
-                html += '<span class="traffic-news-posted">';
-                html += time[0];
-                html += '</span>';
-                html += '</div>';
-                html += '<div class="traffic-rss-message">';
-                html += rs[i].Deviation[0].Message;
-                html += '</div>';
-                html += '<div class="traffic-rss-footer">';
-                html += '</div>';
-                html += '</div>';
+                html +=         '<div class="traffic-rss-header">';
+                html +=             '<span class="traffic-rss-headline">';
+                html +=                 rs[i].Deviation[0].Creator;
+                html +=             '</span>';
+                html +=             '<span class="traffic-news-posted">';
+                html +=                 time[0];
+                html +=             '</span>';
+                html +=         '</div>';
+                html +=         '<div class="traffic-rss-message">';
+                html +=             rs[i].Deviation[0].Message;
+                html +=         '</div>';
+                html +=         '<div class="traffic-rss-footer">';
+                html += '       </div>';
+                html +=     '</div>';
                 //$('#news-rss').append(html);
             }
         },
@@ -45,5 +45,5 @@ function getTrafficNews() {
 }
 
 $(function () {
-    getTrafficNews();
+   //getTrafficNews();
 });
